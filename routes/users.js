4ba870e1598c8403c6da1f70b4b672199a27ee76
king <102906132+Ggy-king@users.noreply.github.com>
2025-03-20 19:59:50 +0800

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,26 @@ const router = express.Router()
 const { postUsersInfo,patchCollectEssay,getCollectEssay } = require('../controller/users')
 const checkTokenMiddleware = require('../middleware/checkTokenMiddleware')
 
+// 简单验证
+router.get('/token',checkTokenMiddleware, (req, res, next) => {
+  res.json({
+    code: '1000',
+    message: 'token验证成功',
+    data: null
+  })
+})
+
+// 注册登录
 router.post('/',(req, res, next) => {
   postUsersInfo(req,res,next)
 })
 
+// 更新收藏
 router.patch('/collect',checkTokenMiddleware, (req, res, next) => {
   patchCollectEssay(req,res,next)
 })
 
+// 拿到收藏列表
 router.get('/collect',checkTokenMiddleware, (req, res, next) => {
   getCollectEssay(req,res,next)
 })
